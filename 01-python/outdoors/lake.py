13ca8f5a56c8d6bd3266107840d278441fbbde1c
junhlei <hacth234@gmail.com>
2020-09-12 05:39:32 -0400

    added lake in lakepy

--- 01-python/outdoors/lake.py
+++ 01-python/outdoors/lake.py
@@ -1,3 +1,14 @@
 def draw_lake():
-    print("lake not found")
+    print(" --------------  ")
+    print("(  . ~This    )")
+    print("(     is  ~   )")
+    print("(  ~   a   .  )")
+    print("(  .  Lake ~  )")
+    print(" --------------  ")
+    print("                  [ No Phishing   ] ")
+    print("                  [   Allowed     ] ")
+    #print("                   ---------------  ")
+    print("                          |          ")
+    print("                          |          ")
+    
     return
