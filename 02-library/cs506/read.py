cf4c220c1a3b8e40568b2e0107bd19f1a6e10c86
junhlei <hacth234@gmail.com>
2020-09-24 19:57:59 -0400

    finished exercise2 with implementing distance formulas and read

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,31 @@
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    #raise NotImplementedError()
+    content = []
+    with open(csv_file_path, "r") as f:
+        lines = f.readlines()        
+        for line in lines:
+            list_strings = line.split(",")
+            list_vals = []
+            for item in list_strings:
+                try:
+                    v = int(item)
+                    list_vals.append(v)
+                except ValueError:
+                    try:
+                        v = float(item)
+                        list_vals.append(v)
+                    except ValueError:
+                        v = rem_quotes(item)
+                        list_vals.append(v)
+            content.append(list_vals)
+    return content
+
+
+def rem_quotes(item):
+    return item.strip("\"")
+    
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,55 @@
+
 def euclidean_dist(x, y):
-    raise NotImplementedError()
-
+    #raise NotImplementedError()
+    if (len(x) != len(y)):
+        return "wrong dimensions"
+    vectorSize = len(x)
+    sum = 0
+    for ind in range(vectorSize):
+        sum += (x[ind] - y[ind])**2
+    return sum**(0.5)
+    
 def manhattan_dist(x, y):
-    raise NotImplementedError()
-
+    #raise NotImplementedError()
+    if (len(x) != len(y)):
+        return "wrong dimensions"
+    vectorSize = len(x)
+    sum = 0
+    for ind in range(vectorSize):
+        sum += abs(x[ind] - y[ind])
+    return sum
 def jaccard_dist(x, y):
-    raise NotImplementedError()
-
+    #raise NotImplementedError()
+    if (x == y):
+        return 0
+    a = set(x)
+    b = set(y)
+    
+    #1 - jaccard index
+    intersection_card = len(a.intersection(b))
+    union_card = len(a.union(b))
+    return 1 - intersection_card/union_card
+    
 def cosine_sim(x, y):
-    raise NotImplementedError()
-
+    #raise NotImplementedError()
+    if (len(x) != len(y)):
+        return "wrong dimensions"
+        
+    normedY = norm(y)
+    normedX = norm(x)
+    if (normedY == 0 or normedX == 0):
+        return 1
+    return dot(x, y)/(normedX*normedY)
+    
 # Feel free to add more
+
+def dot(v1, v2):
+    #took for stackoverflow
+    return sum(x*y for x,y in zip(v1,v2))
+    
+def norm(field):
+    sum = 0
+    for i in field:
+        sum += i**2
+    return sum**(1/2)
+    
